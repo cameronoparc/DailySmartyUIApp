cd8ac200e9e7de5769fd749e3242503b9043308c
Cameron Crepeaux <cameroncrapo@overstock.com>
2020-05-21 20:18:04 -0600

    setup app routes

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,22 +3,24 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter } from "react-router-dom";
-import App from "./components/app";
+import App from "./components/home";
 import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 import "./style/main.scss";
 
+import Home from "./components/home";
+
 function main() {
-  ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>,
-    document.querySelector(".app-wrapper")
-  );
+	ReactDOM.render(
+		<Provider store={createStoreWithMiddleware(reducers)}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>,
+		document.querySelector(".app-wrapper")
+	);
 }
 
 document.addEventListener("DOMContentLoaded", main);
