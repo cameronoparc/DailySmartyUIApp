15096d0b16187ac57b53d89507cfb55017042646
Cameron Crepeaux <cameroncrapo@overstock.com>
2020-05-22 14:48:37 -0600

    used axios to perform get request to fetch recents posts and setup the required reducer

---
+++ src/reducers/postsReducer.js
@@ -0,0 +1,14 @@
+import { SET_RECENT_POSTS } from "../actions/types";
+
+const INIT_STATE = {
+	posts: [],
+	recentPosts: []
+};
+
+export default function (state = INIT_STATE, action) {
+	switch (action.type) {
+		case SET_RECENT_POSTS:
+			return [...state, (recentPosts: action.payload)];
+			deafult: return state;
+	}
+}
