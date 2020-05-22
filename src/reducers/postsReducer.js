6c9174288447b9ee2529ea1a7e1c1fc6c9908b7b
Cameron Crepeaux <cameroncrapo@overstock.com>
2020-05-22 17:50:49 -0600

    added SET_RESULTS_POSTS reducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,6 +1,6 @@
-import { SET_RECENT_POSTS } from "../actions/types";
+import { SET_RECENT_POSTS, SET_RESULTS_POSTS } from "../actions/types";
 const INIT_STATE = {
-	posts: [],
+	resultsPosts: [],
 	recentPosts: []
 };
 export default function (state = INIT_STATE, action) {
@@ -10,6 +10,12 @@ export default function (state = INIT_STATE, action) {
 			return {
 				...state,
 				recentPosts
+			};
+		case SET_RESULTS_POSTS:
+			const resultsPosts = action.payload;
+			return {
+				...state,
+				resultsPosts
 			};
 		default:
 			return state;
