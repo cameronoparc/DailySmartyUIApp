63750d2c73108363a96587df0def8ea05ec66360
Cameron Crepeaux <cameroncrapo@overstock.com>
2020-05-22 17:45:30 -0600

    built init resultsposts component

--- src/components/results.js
+++ src/components/results.js
@@ -6,6 +6,8 @@ import SearchBar from "./searchBar";
 import { connect } from "react-redux";
 import * as actions from "../actions";
 
+import ResultsPosts from "./resultsPosts";
+
 class Results extends Component {
 	handleSearchBarSubmit(query) {
 		this.props.fetchPostsWithQuery(query);
@@ -16,8 +18,10 @@ class Results extends Component {
 			<div>
 				<Logo size={55} />
 				<SearchBar onSubmit={query => this.handleSearchBarSubmit(query)} />
+				<ResultsPosts />
 			</div>
 		);
 	}
 }
+
 export default connect(null, actions)(Results);
